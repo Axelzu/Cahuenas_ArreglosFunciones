fbe2c31ce50408de461e4036c548b9a76cdc53b4
Alexander Cahueñas <Alexander Cahueas>
2023-12-03 12:06:06 -0500

    2

--- suma.c
+++ suma.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+//aqui es para inicializar la matriz con numeros aleatorios entre o y 100
+void inicializarMatriz(int filas, int columnas, int matriz[filas][columnas]) {
+    srand(time(NULL));
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = rand() % 101;
+        }
+    }
+}
